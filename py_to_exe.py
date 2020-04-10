cbe3b9b9ef16fb29048437f7a849a9e2e364f72b
Naotonosato <33174108+Naotonosato@users.noreply.github.com>
2020-04-10 16:08:15 +0900

    add "-O3" option

--- py_to_exe.py
+++ py_to_exe.py
@@ -7,7 +7,7 @@ os.system("cython --embed "+filename)
 
 c_source = os.path.splitext(filename)[0]+".c"
 
-options = "\"-I`python3-config --prefix`/Headers\" `python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('LIBS'),distutils.sysconfig.get_config_var('SYSLIBS'),distutils.sysconfig.get_config_var('LINKFORSHARED'))\"`"
+options = "\"-I`python3-config --prefix`/Headers\" `python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('LIBS'),distutils.sysconfig.get_config_var('SYSLIBS'),distutils.sysconfig.get_config_var('LINKFORSHARED'))\"` -O3"
 
 cc = "`python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('CC'))\"`"
 
