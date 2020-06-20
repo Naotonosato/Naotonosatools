7ec45169f0f6fded7626c58032e517d5ba82d353
Naotonosato <33174108+Naotonosato@users.noreply.github.com>
2020-06-20 19:21:49 +0900

    output filename

--- py_to_exe.py
+++ py_to_exe.py
@@ -7,7 +7,9 @@ os.system("cython --embed "+filename)
 
 c_source = os.path.splitext(filename)[0]+".c"
 
-options = "\"-I`python3-config --prefix`/Headers\" `python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('LIBS'),distutils.sysconfig.get_config_var('SYSLIBS'),distutils.sysconfig.get_config_var('LINKFORSHARED'))\"` -O3"
+output_filename = os.path.splitext(filename)[0]+".out"
+
+options = f"\"-I`python3-config --prefix`/Headers\" `python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('LIBS'),distutils.sysconfig.get_config_var('SYSLIBS'),distutils.sysconfig.get_config_var('LINKFORSHARED'))\"` -O3 -o {output_filename}"
 
 cc = "`python3 -c \"import distutils.sysconfig;print(distutils.sysconfig.get_config_var('CC'))\"`"
 
